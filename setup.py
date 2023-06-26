f6aff7c4664d3ee0404e9cbd48ff9065a67a032e
Noa Shinitski <noa.shinitski@gmail.com>
2023-06-26 14:17:46 +0200

    Add a setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyaldi',
+      version='0.1',
+      description='An implementation of ALDI sampler. See README for details.',
+      url='https://github.com/noashin/PyALDI',
+      author='Noa Malem Shinitski',
+      author_email='noa.shinitski@gmail.com',
+      license='MIT',
+      packages=['pyaldi'],
+      zip_safe=False)
